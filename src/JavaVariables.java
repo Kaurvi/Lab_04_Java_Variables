a458039870518616a3daadb3d8a2435007bf56de
Kaurvi <kaurvi@mail.uc.edu>
2023-06-04 19:55:44 -0400

    added the code

---
+++ src/JavaVariables.java
@@ -0,0 +1,19 @@
+
+public class JavaVariables
+{
+    public static void main(String[] args)
+    {
+        double intOperandA = 1.2;
+        double intOperandB = 3.6;
+        double sum = intOperandA + intOperandB;
+        double product = intOperandA*intOperandB;
+        double difference = intOperandA-intOperandB;
+        double quotient = intOperandB / intOperandA ;
+        double modulo = intOperandB%intOperandA;
+        System.out.println(" The sum of intOperandA and intOperaB is" + " " +sum);
+        System.out.println(" The product of intOperandA and intpOerandB is" +" "+product);
+        System.out.println("The difference of intOperandA and intOperaB is"+" "+difference);
+        System.out.println("The division of intOperandA and intOperandB is" +" " +quotient);
+        System.out.println(" The modulo calculation of intOperandA and intOperandB is"+" "+modulo);
+    }
+}
